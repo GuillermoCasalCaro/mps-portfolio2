9ed2c1c4a7a570aeeceae4249d10b97b50cac357
Guillermo <moni320097@gmail.com>
2025-06-14 09:01:43 +0200

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://astro-resume-theme.netlify.app",
-  integrations: [tailwind(), icon(), mdx(), sitemap()]
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,39 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: "var(--color-primary)",
-				secondary: "var(--color-secondary)",
-			},
-			textColor: {
-				default: "var(--color-text)",
-				offset: "var(--color-text-offset)",
-				link: "var(--color-link)",
-				marking: "var(--color-marking)", 
-			},
-			backgroundColor: {
-				default: "var(--color-background)",
-				offset: "var(--color-background-offset)",
-				border: "var(--color-border)",
-				start: "var(--color-body-background-start)",
-				end: "var(--color-body-background-end)",
-				modal: "var(--color-modal-background)",
-			},
-			backgroundImage: {
-				'soft-gradient': 'linear-gradient(to bottom right, #f9fafb, #ffffff)',
-			},
-			borderColor: {
-				default: "var(--color-border)",
-			},
-			animation: {
-				"spin-slower": "spin 35s ease infinite",
-				"spin-slow": "spin 25s ease-in-out infinite reverse",
-			},
-		}
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+        link: "var(--color-link)",
+        marking: "var(--color-marking)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+        border: "var(--color-border)",
+        start: "var(--color-body-background-start)",
+        end: "var(--color-body-background-end)",
+        modal: "var(--color-modal-background)",
+      },
+      backgroundImage: {
+        "soft-gradient": "linear-gradient(to bottom right, #f9fafb, #ffffff)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+      animation: {
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
